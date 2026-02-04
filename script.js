bad1d63abf0cd2db446cccf97de0b2f063fb58a8
wanednamarissa05-maker <wanednamarissa05@gmail.com>
2026-02-04 18:12:22 +0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+const noBtn = document.getElementById("noBtn");
+const yesBtn = document.getElementById("yesBtn");
+const message = document.getElementById("message");
+
+noBtn.addEventListener("mouseover", () => {
+  const x = Math.random() * 200 - 100;
+  const y = Math.random() * 200 - 100;
+  noBtn.style.transform = `translate(${x}px, ${y}px)`;
+});
+
+yesBtn.addEventListener("click", () => {
+  message.textContent = "Yayyy 💕 I knew you’d say yes!";
+});
